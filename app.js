b2b208a43a6ccadb49b2cb47291459c82ead3c41
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2024-10-11 22:18:15 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const morgan = require("morgan");
+const express = require("express");
+const usersRoute = require("./routes/usersRoute");
+const carsRoute = require("./routes/carsRoute");
+const sparepartsRoute = require("./routes/sparepartsRoute");
+const driverRoutes = require("./routes/driverRoute");
+
+const app = express();
+const port = 3000;
+
+// Reading json from body (client)
+app.use(express.json());
+
+// midleware LOGGING : third party package
+app.use(morgan());
+
+//midleware example
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+
+  next();
+});
+
+//logging basic
+app.use((req, res, next) => {
+  req.username = "FSW-2";
+
+  next();
+});
+
+// Health Check
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json({
+      status: "Succeed",
+      message: "Ping successfully",
+      isSuccess: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: "Ping failed",
+      isSuccess: false,
+      error: error.message,
+    });
+  }
+});
+
+// Routes
+app.use("/api/v1/users", usersRoute);
+app.use("/api/v1/cars", carsRoute);
+app.use("/api/v1/spareparts", sparepartsRoute);
+app.use("/api/v1/drivers", driverRoutes);
+
+// Middleware to handle page not found
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: "Failed",
+    message: "API not found !",
+    isSuccess: false,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
---
+++ midleware/upload.js
@@ -0,0 +1,16 @@
+const multer = require("multer");
+
+const multerFiltering = (req, file, cb) => {
+  if (file.mimetype == "image/png" || file.mimetype == "image/jpeg") {
+    cb(null, true); 
+  } else {
+    cb(new Error("image format not valid..."));
+  }
+};
+
+const upload = multer({
+  fileFilter: multerFiltering,
+  // dest: "public/images/users",
+});
+
+module.exports = upload;
